a476fd8ad79b08e82e0ff6a549185fac65a96f89
fortyone-41 <75414851+fortyone-41@users.noreply.github.com>
2021-05-14 13:46:35 +0700

    rewrite infojs

--- src/Modules/Content/Info/Info.js
+++ src/Modules/Content/Info/Info.js
@@ -9,9 +9,9 @@ const Info = () => {
     return (
         <div className="info">
 
-            <div className="card-container">
-                <Tabs type="card">
-                    <TabPane tab="Биография" style={{backgroundImage: "linear-gradient(to top, rgb(250, 250, 249), rgb(194, 194, 174))"}} key="1">
+            <div className="card-container" >
+                <Tabs type="card" >
+                    <TabPane tab="Биография"  key="1">
                         <div>
                         <h1>Биография</h1>
                         <h2>
@@ -19,22 +19,32 @@ const Info = () => {
                             <p>Дата рождения: 28.11.1995</p>
                             <p>Образование: Высшее(Информационные системы и технологии)</p> 
                             <p>"ФГБВОУ ВО Норильский Государственный Индустриальный Институт" г.Норильск</p>
+                            <p></p>
+                            <p></p>
+                            <p style={{fontSize: "22px", font: "bold"}}>Детство:</p>
+                            <p>В программирование затянуло еще с раннего детства.  </p>
+                            <p>Примерно в 14 лет администрировал игровой сервер, и появилась нужда в информацинном сайте, приобщенному к серверу.</p>
+                            <p>Изучил структуру HTML, и основы CSS</p>
+                            <p></p>
+                            <p></p>
+                            <p style={{fontSize: "22px", font: "bold"}}>Школа:</p>
+                            <p>Уже в школе начался Pascal, который затянул меня окончательно, и годовая программа уложилась в 3 недели.</p>
                         </h2>
                         </div>
                     </TabPane>
-                    <TabPane tab="Опыт работы" style={{backgroundImage: "linear-gradient(to top, rgb(250, 250, 249), rgb(194, 194, 174))"}} key="2">
-                        <Timeline mode="alternate">
-                            <Timeline.Item>После окончания учебы, с 07.2017 работал фрилансером</Timeline.Item>
-                            <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />} color="green">05.05.2018 Устроился на работу в "ЗФ" НН-ОЦО</Timeline.Item>
+                    <TabPane tab="Опыт работы"  key="2">
+                        <Timeline mode="alternate" >
+                            <Timeline.Item >После окончания учебы, работал на фриланс основе, оказывая услуги Web-программирования</Timeline.Item>
+                            <Timeline.Item color="yellow">Начал трудовую деятельность в "ЗФ" НН-ОЦО</Timeline.Item>
                             <Timeline.Item >
                                 Занимался обслуживанием АРМ, установка/настройка ПО, администрирование корпоративной сети передачи данных.
                                 
                         </Timeline.Item>
                             <Timeline.Item>В процессе разработал проект бесшовной Wi-Fi сети, с использованием Radius-сервера.</Timeline.Item>
                             <Timeline.Item>Прошел обучающий курс по использованию сетевого оборудования CISCO</Timeline.Item>
-                            <Timeline.Item color="red">Уволился 16.09.2019</Timeline.Item>
-                            <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}  color="green">
-                                17.09.2019 Устроился на работу в ООО "Норильскникельремонт" МПО "Норильскавтоматика"
+                            <Timeline.Item color="red">Прекратил трудовую деятельность в данной компании</Timeline.Item>
+                            <Timeline.Item color="yellow">
+                                Начал трудовую деятельность в ООО "Норильскникельремонт" МПО "Норильскавтоматика"
                         </Timeline.Item>
                         <Timeline.Item>Занимался администрированием SCADA-системы.</Timeline.Item>
                         <Timeline.Item>Разработал Web-систему приема заявок о неисправностях SCADA-системы.</Timeline.Item>
